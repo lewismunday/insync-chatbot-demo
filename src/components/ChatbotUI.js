15fac8843581d5e360a51b1aae2cecee1f57aa37
Lewis Munday <lewisjohnmunday@gmail.com>
2023-05-10 19:48:42 +0100

    UI updated

--- src/components/ChatbotUI.js
+++ src/components/ChatbotUI.js
@@ -1,6 +1,8 @@
 import React, {useEffect, useRef} from 'react';
 import '../App.css';
 import {context} from '../tools/utils';
+import {ThreeDots} from 'react-loader-spinner'
+
 
 
 // OpenAI Configuration
@@ -31,12 +33,16 @@ const ChatbotUI = () => {
         { message: 'Hi there! I\'m Ila, a chatbot for Insync Insurance! How can I help you today?', user: false }
     ]);
     const [inputValue, setInputValue] = React.useState('');
+    let [isLoading, setIsLoading] = React.useState(false);
+    const [model, setModel] = React.useState('gpt-3.5-turbo');
 
     const fillQuestions = (e) => {
         setInputValue(e.target.innerText);
     }
 
     const handleSubmit = async event => {
+        if (!inputValue) return;
+        setIsLoading(true)
         event.preventDefault();
         const newMessages = [...messages, { message: inputValue, user: true }];
         setMessages(newMessages);
@@ -69,13 +75,17 @@ const ChatbotUI = () => {
             Ila:`
 
         const response = await openai.createChatCompletion({
-            model: "gpt-4",
+            model: model,
             messages: [{role: "user", content: prompt}],
         });
+
+
+        console.log(response)
 
         setMessages([...newMessages, {message: response.data.choices[0].message.content, user: false}]);
         // Hide typing indicator
         document.querySelector('.typing').style.display = 'none';
+        setIsLoading(false)
     };
 
     function onTextAreaEnterPress(e) {
@@ -90,10 +100,25 @@ const ChatbotUI = () => {
         bottomRef.current?.scrollIntoView({behavior: 'smooth'});
     }, [messages]);
 
+    const handleModelChange = (event) => {
+        setModel(event.target.value);
+    };
+
     return (
         <div>
         <div className="chat-container">
             <div className="chat-header">
+                <div className="App">
+                    <p>Select response type <i>(for demo purposes only)</i> :</p>
+                    <div>
+                        <form>
+                            <select name="model" id="model" value={model} onChange={handleModelChange}>
+                                <option value="gpt-3.5-turbo">Faster, less enhanced responses</option>
+                                <option value="gpt-4">Slower, more enhanced responses</option>
+                            </select>
+                        </form>
+                    </div>
+                </div>
                 <h2>Insync Chatbot</h2>
             </div>
             <div className="chat-body">
@@ -101,7 +126,16 @@ const ChatbotUI = () => {
                     <ChatMessage key={index} message={message.message} user={message.user} />
                 ))}
             </div>
-            <p className="typing" style={{display: "none"}}>Ila is typing...</p>
+            <p className="typing" style={{display: "none"}}><ThreeDots
+                height="40"
+                width="40"
+                color="#ff7401"
+                ariaLabel="grid-loading"
+                radius="12.5"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={isLoading}
+            /></p>
             <div ref={bottomRef} />
         </div>
             <form onSubmit={handleSubmit} className="App">
@@ -113,10 +147,12 @@ const ChatbotUI = () => {
                     value={inputValue}
                     onChange={event => setInputValue(event.target.value)}
                     onKeyDown={onTextAreaEnterPress} // submit form when Enter key is pressed
+                    disabled={isLoading}
                 />
                 {/*<button type="submit">Send</button>*/}
             </form>
             <div className="App">
+                <p style={{fontSize: 'small', color: 'red'}}>{model === "gpt-4" ? 'Please note, response time can take up to 60 seconds due to restricted server bandwidth.' : null}</p>
                 <h3>Click the button below to see an example of how Ila can help you</h3>
                 <button type="submit" onClick={fillQuestions}>Tell me about Insync Insurance</button>
                 <button type="submit" onClick={fillQuestions}>Tell me about D&O insurance</button>
